c269ae36277a9d7367e28ccf1160b508e25951e3
wh1pch81n <wh1pch81n@gmail.com>
2014-07-21 19:19:19 -0700

    Initial Commit

---
+++ ClassicPhotos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassicPhotos
+//
+//  Created by Derrick Ho on 7/21/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassicPhotosTests/ClassicPhotosTests.m
@@ -0,0 +1,34 @@
+//
+//  ClassicPhotosTests.m
+//  ClassicPhotosTests
+//
+//  Created by Derrick Ho on 7/21/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClassicPhotosTests : XCTestCase
+
+@end
+
+@implementation ClassicPhotosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
