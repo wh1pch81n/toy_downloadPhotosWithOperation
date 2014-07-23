0c5dcbfa14bb68cc005886f77261df34e1e93974
wh1pch81n <wh1pch81n@gmail.com>
2014-07-22 20:06:47 -0700

    loads the images in the table view via NSOperations

--- ClassicPhotos/ListViewController.h
+++ ClassicPhotos/ListViewController.h
@@ -7,12 +7,41 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreImage/CoreImage.h>
+#pragma  mark - 1
+/**
+ You can delete the importation of core image since that work has been moved.
+ However, you still ned to import PhotoRecord, PendingOperations, ImageDownloader, and ImageFiltration
+ */
+//#import <CoreImage/CoreImage.h> //Not needed here anymore since the NSOperation takes care of it
+#import "PhotoRecord.h"
+#import "PendingOperations.h"
+#import "ImageDownloader.h"
+#import "ImageFiltration.h"
 
-#define kDatasourceURLString @"http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist"
+#pragma mark - 2
+//import the afnetworking
+#import "AFNetworking.h"
 
-@interface ListViewController : UITableViewController
 
-@property (strong, nonatomic) NSDictionary *photos;// main datasource for our images
+#define kDatasourceURLStringOld @"http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist"
+#define kDatasourceURLString @"https://sites.google.com/site/soheilsstudio/tutorials/nsoperationsampleproject/ClassicPhotosDictionary.plist"
+
+#pragma mark - 3
+/**
+ attach delegates
+ */
+@interface ListViewController : UITableViewController <ImageDownloaderDelegate, ImageFiltrationDelegate>
+
+#pragma mark - 4
+/**
+ Instances of PhotoRecord
+ */
+@property (nonatomic, strong) NSMutableArray *photos; //main datasource of controller
+
+#pragma mark - 5
+/**
+ Use this to track pending operations
+ */
+@property (nonatomic, strong) PendingOperations *pendingOperations;
 
 @end
--- ClassicPhotos/ListViewController.m
+++ ClassicPhotos/ListViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ListViewController.h"
+#import "AFNetworking/AFNetworking.h"
 
 @interface ListViewController ()
 
@@ -16,10 +17,85 @@
 
 #pragma mark - lazy instantiation
 
-- (NSDictionary *)photos {
+- (PendingOperations *)pendingOperations {
+    return _pendingOperations ?: (_pendingOperations = [PendingOperations new]);
+}
+
+- (NSMutableArray *)photos {
     if(_photos == nil) {
+#pragma mark - 1
+        /**
+         Create a NSURL and a NSURLRequest to point to the location of the data source
+         */
         NSURL *dataSourceURL = [NSURL URLWithString:kDatasourceURLString];
-        _photos = [NSDictionary dictionaryWithContentsOfURL:dataSourceURL];
+        NSURLRequest *request = [NSURLRequest requestWithURL:dataSourceURL];
+        
+#pragma mark - 2
+        /**
+         create AFHTTPRequestOperation object with request
+         */
+        AFHTTPRequestOperation *datasource_download_operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
+#pragma mark - 3
+        /**
+         Tell the user that some network activity is going on by turning on the spinning gear thing
+         */
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+        
+#pragma mark - 4
+        /**
+         use two blocks to determine what happens if it succeeds or fails
+         */
+        [datasource_download_operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+#pragma  mark - 5
+            /**
+             convert the plist into data and then to a dictionary
+             */
+            NSData *datasource_data = (NSData *)responseObject;
+            CFPropertyListRef plist = CFPropertyListCreateFromXMLData(kCFAllocatorDefault,
+                                                                      (__bridge CFDataRef)datasource_data,
+                                                                      kCFPropertyListImmutable, NULL);
+            
+            NSDictionary *datasource_dictionary = (__bridge NSDictionary *)plist;
+            
+#pragma mark - 6
+            /**
+             create an array of PhotoRecords
+             */
+            NSMutableArray *records = [NSMutableArray array];
+            for (NSString *key in datasource_dictionary) {
+                PhotoRecord *record = [[PhotoRecord alloc] init];
+                [record setUrl:[NSURL URLWithString:[datasource_dictionary objectForKey:key]]];
+                [record setName:key];
+                [records addObject:record];
+            }
+#pragma mark - 7
+            /**
+             set the photo array and also release the plist
+             */
+            self.photos = records;
+            
+            CFRelease(plist);
+            
+            [self.tableView reloadData];
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+#pragma mark - 8
+            // Connection error message
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!"
+                                                            message:error.localizedDescription
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            alert = nil;
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        }];
+#pragma mark - 9
+        /**
+         add it to the download queue
+         */
+        [self.pendingOperations.downloadQueue addOperation:datasource_download_operation];
     }
     return _photos;
 }
@@ -32,6 +108,7 @@
     self.tableView.rowHeight = 80;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setPendingOperations:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -58,21 +135,35 @@
     if(!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdentifier];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+#pragma mark - 1
+        UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        cell.accessoryView = activityIndicatorView;
+    }
+#pragma mark - 2
+    PhotoRecord *aRecord = [self.photos objectAtIndex:indexPath.row];
+    
+#pragma mark - 3
+    if (aRecord.hasImage) {
+        [((UIActivityIndicatorView *)cell.accessoryView) stopAnimating];
+        cell.imageView.image = aRecord.image;
+        cell.textLabel.text = aRecord.name;
     }
     
-    NSString *rowKey = self.photos.allKeys[indexPath.row];
-    NSURL *imageURL = [NSURL URLWithString:self.photos[rowKey]];
-    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-    UIImage *image;
-    
-    if (imageData) {
-        UIImage *unfiltered_image = [UIImage imageWithData:imageData];
-        image = [self applySepiaFilterToImage:unfiltered_image];
+#pragma mark - 4
+    else if (aRecord.isFailed) {
+        [((UIActivityIndicatorView *)cell.accessoryView) stopAnimating];
+        cell.imageView.image = [UIImage imageNamed:@"Failed.png"];
+        cell.textLabel.text = @"Failed to load";
     }
+#pragma mark - 5
+    else {
+        [((UIActivityIndicatorView *)cell.accessoryView) startAnimating];
+        cell.imageView.image = [UIImage imageNamed:@"Placeholder.png"];
+        cell.textLabel.text = @"";
+        [self startOperationsForPhotoRecord:aRecord atIndexPath:indexPath];
     
-    cell.textLabel.text = rowKey;
-    cell.imageView.image = image;
-    
+    }
     return cell;
 }
 
@@ -89,6 +180,64 @@
     sepiaImage = [UIImage imageWithCGImage:outputImageRef];
     CGImageRelease(outputImageRef);
     return sepiaImage;
+}
+
+
+#pragma mark - operations
+
+- (void)startOperationsForPhotoRecord:(PhotoRecord *)record atIndexPath:(NSIndexPath *)indexPath {
+    if (!record.hasImage) {
+        [self startImageDownloadingForRecord:record atIndexPath:indexPath];
+    }
+    
+    if (!record.isFiltered) {
+        [self startImageFiltrationForRecord:record atIndexPath:indexPath];
+    }
+}
+
+
+- (void)startImageDownloadingForRecord:(PhotoRecord *)record atIndexPath:(NSIndexPath *)indexPath {
+    if(![self.pendingOperations.downloadsInProgress.allKeys containsObject:indexPath]) {
+        ImageDownloader *imageDownloader = [[ImageDownloader alloc] initWithPhotoRecord:record atIndexpath:indexPath delegate:self];
+        [self.pendingOperations.downloadsInProgress setObject:imageDownloader forKey:indexPath];
+        [self.pendingOperations.downloadQueue addOperation:imageDownloader];
+    }
+}
+
+- (void)startImageFiltrationForRecord:(PhotoRecord *)record atIndexPath:(NSIndexPath *)indexPath {
+    // 3
+    if (![self.pendingOperations.filtrationsInProgress.allKeys containsObject:indexPath]) {
+        
+        // 4
+        // Start filtration
+        ImageFiltration *imageFiltration = [[ImageFiltration alloc] initWithPhotoRecord:record atIndexPath:indexPath delegate:self];
+        
+        // 5
+        ImageDownloader *dependency = [self.pendingOperations.downloadsInProgress objectForKey:indexPath];
+        if (dependency)
+            [imageFiltration addDependency:dependency];
+        
+        [self.pendingOperations.filtrationsInProgress setObject:imageFiltration forKey:indexPath];
+        [self.pendingOperations.filtrationQueue addOperation:imageFiltration];
+    }
+}
+
+#pragma mark - Delegate methods
+
+- (void)imageDownloaderDidFinish:(ImageDownloader *)downloader {
+    
+    // 1
+    NSIndexPath *indexPath = downloader.indexPathInTableView;
+    // 2
+    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    // 3
+    [self.pendingOperations.downloadsInProgress removeObjectForKey:indexPath];
+}
+
+- (void)imageFiltrationDidFinish:(ImageFiltration *)filtration {
+    NSIndexPath *indexPath = filtration.indexPathInTableView;
+    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    [self.pendingOperations.filtrationsInProgress removeObjectForKey:indexPath];
 }
 
 @end
